b730356dbfcf0d056d18eef59f33fde0b7eb7809
cemÄilcakmak <b.cemilcakmak@gmail.com>
2021-11-21 23:42:44 +0300

    transferred mongodb connect url into .env due to security

--- db/connect.js
+++ db/connect.js
@@ -1,11 +1,7 @@
 const mongoose = require("mongoose");
 
-//if there is a space between mongodb params, causes an error.
-const connnectionString =
-  "mongodb+srv://nodejscamp:buzGBDSJuotd39nf@nodeexpressprojects.iatja.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority;";
-
 const connectDB = (url) => {
-    return mongoose.connect(connnectionString);
+    return mongoose.connect(url, connnectionString);
 }
 
 module.export = connectDB;
